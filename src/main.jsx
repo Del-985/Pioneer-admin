13e220e18ad40596fd4060522e53ee8c4c4c2547
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-09-09 14:15:57 -0400

    Load website CMS styles

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { CompanyProvider } from './context/CompanyContext.jsx';
 import './styles.css';
 import './management.css';
 import './features.css';
+import './website-cms.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

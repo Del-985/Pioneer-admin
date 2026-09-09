7de7a0dd800802016c96f849c9d7e2fdd6dbe280
Del-985 <119140366+Del-985@users.noreply.github.com>
2026-09-09 16:56:22 -0400

    Remove website CMS stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import { CompanyProvider } from './context/CompanyContext.jsx';
 import './styles.css';
 import './management.css';
 import './features.css';
-import './website-cms.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
